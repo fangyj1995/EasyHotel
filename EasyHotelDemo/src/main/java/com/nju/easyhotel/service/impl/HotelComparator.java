d1c69fbde56f023742c0296ed0843f95928bc432
qqjkl7 <380890432@qq.com>
2016-11-10 20:21:11 +0800

    rename

---
+++ EasyHotelDemo/src/main/java/com/nju/easyhotel/service/impl/HotelComparator.java
@@ -0,0 +1,41 @@
+package com.nju.easyhotel.service.impl;
+
+import java.util.Comparator;
+
+import com.nju.easyhotel.vo.HotelSearchResultVo;
+public class HotelComparator{
+   public static Comparator<HotelSortInfo> price(){
+	   return new Comparator<HotelSortInfo>(){
+
+		@Override
+		public int compare(HotelSortInfo o1, HotelSortInfo o2) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		   
+	   };
+   }
+   public static Comparator<HotelSortInfo> rate(){
+	   return new Comparator<HotelSortInfo>(){
+
+		@Override
+		public int compare(HotelSortInfo o1, HotelSortInfo o2) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		   
+	   };
+   }
+   public static Comparator<HotelSortInfo> starLevel(){
+	   return new Comparator<HotelSortInfo>(){
+
+		@Override
+		public int compare(HotelSortInfo o1, HotelSortInfo o2) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+		   
+	   };
+   }
+}
+
