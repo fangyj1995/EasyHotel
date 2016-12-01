cd77d6f3edb936178bf3ba16e0bfc573f85b1ff1
fangyj1995 <380890432@qq.com>
2016-12-01 15:46:13 +0800

    commit

--- easyhotel/src/main/java/com/nju/easyhotel/service/hotelServiceImpl/SearchDeal.java
+++ easyhotel/src/main/java/com/nju/easyhotel/service/hotelServiceImpl/SearchDeal.java
@@ -1,6 +1,7 @@
 package com.nju.easyhotel.service.hotelServiceImpl;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -48,3 +49,32 @@ public class SearchDeal {
     	return list;
     }
 }
+class HotelComparator{
+	   public static Comparator<HotelSortInfo> price(){
+		   return new Comparator<HotelSortInfo>(){
+			public int compare(HotelSortInfo o1, HotelSortInfo o2) {
+				if(o1.price()==o2.price()) return 0;
+				if(o1.price()<o2.price()) return -1;
+				return 1;
+			}		   
+		   };
+	   }
+	   public static Comparator<HotelSortInfo> rate(){
+		   return new Comparator<HotelSortInfo>(){
+			public int compare(HotelSortInfo o1, HotelSortInfo o2) {
+				if(o1.rate()==o2.rate()) return 0;
+				if(o1.rate()>o2.rate()) return -1;
+				return 1;
+			}		   
+		   };
+	   }
+	   public static Comparator<HotelSortInfo> starLevel(){
+		   return new Comparator<HotelSortInfo>(){
+			public int compare(HotelSortInfo o1, HotelSortInfo o2) {
+				if(o1.level()==o2.level()) return 0;
+				if(o1.level()>o2.level()) return -1;
+				return 1;
+			}		   
+		   };
+	   }
+	}
